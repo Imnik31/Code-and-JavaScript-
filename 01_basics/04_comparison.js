e768b6dbe42cb9a3f1cf924febd8e9317a7d27f6
Nikhi Katiyar <nikhilkatiyar0507@gmail.com>
2024-04-04 14:10:06 +0530

    comparision

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -4,8 +4,8 @@
 
 console.log(null>0);
 console.log(null==0);
-console.log(null>=0);
+console.log(null>=0);   // comparisons convert null to a number , treating it as 0 
 
 //strict cheak ===
 
-console.log("2"===2);
+console.log("2"===2);  // check with datatype
