cc4c318b42529d303951af279aa694824d43c112
Nikhi Katiyar <nikhilkatiyar0507@gmail.com>
2024-04-09 19:30:08 +0530

    OOPS concept

--- 09_classes_oops/05_oops.js
+++ 09_classes_oops/05_oops.js
@@ -1,3 +1,5 @@
+//object litrals
+
 const user = {
     username: "hitesh",
     loginCount: 8,
@@ -5,7 +7,7 @@ const user = {
 
     getUserDetails: function(){
         //console.log("Got user details from database");
-        // console.log(`Username: ${this.username}`);
+        // console.log(`Username: ${this.username}`);              // this means current context
         console.log(this);
     }
 
@@ -15,8 +17,10 @@ const user = {
 
 //console.log(user.username)
 //console.log(user.getUserDetails());
-// console.log(this);
+// console.log(this);          // when we use this in global object it willgive us {}, but when we use this in browser it will give window object
 
+
+const promiseOne= new Promise()         // new is the constructor function constructor fucntion gives us a new instance everytime
 
 function User(username, loginCount, isLoggedIn){
     this.username = username;
@@ -31,7 +35,7 @@ function User(username, loginCount, isLoggedIn){
     return this
 }
 
-const userOne = new User("hitesh", 12, true)
+const userOne = new User("nikhil", 12, true)
 const userTwo = new User("ChaiAurCode", 11, false)
-console.log(userOne.constructor);
+console.log(userOne.constructor);                           // constructor is property which is reference for itself like User
 //console.log(userTwo);
